80949c9b21ee4711bfe211bdf5c546ef09470afa
lucascalsilva <lucasc.alm.silva@gmail.com>
2020-10-01 18:55:53 +0200

    Changed pom to use Camunda CE for testing

--- src/test/java/com/camunda/training/paymentservice/PaymentProcessTest.java
+++ src/test/java/com/camunda/training/paymentservice/PaymentProcessTest.java
@@ -1,14 +1,18 @@
 package com.camunda.training.paymentservice;
 
+import org.camunda.bpm.engine.externaltask.LockedExternalTask;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.camunda.bpm.engine.test.Deployment;
 import org.camunda.bpm.engine.test.ProcessEngineRule;
+import org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests;
 import org.camunda.bpm.extension.process_test_coverage.junit.rules.TestCoverageProcessEngineRule;
 import org.camunda.bpm.extension.process_test_coverage.junit.rules.TestCoverageProcessEngineRuleBuilder;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
+
+import java.util.List;
 
 import static org.camunda.bpm.engine.test.assertions.bpmn.AbstractAssertions.init;
 import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.*;
@@ -34,4 +38,24 @@ public class PaymentProcessTest {
         complete(externalTask());
         assertThat(processInstance).isEnded().variables().containsEntry("paymentOK", true);
     }
+
+    public void exampleOfLockingTasks(){
+        //Handling a bpmn error to the flow in the charge credit card task
+        List<LockedExternalTask> lockedTask = externalTaskService().fetchAndLock(1, "test")
+                .topic("credit-card-charging", 10000L).execute();
+
+        lockedTask.forEach(lockedExternalTask -> {
+            externalTaskService().handleBpmnError(lockedExternalTask.getId(), "test", "ChargeFailed",
+                    "Error message");
+        });
+    }
+
+    public void exampleOfStartingFromAnotherPoint(){
+        //Start process after the charge credit task
+        runtimeService().createProcessInstanceModification("PaymentProcess").startAfterActivity("ChargeCreditTask")
+                .setVariable("creditSufficient", false).execute();
+    }
+
+
+
 }
